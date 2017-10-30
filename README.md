f1d80f04777c5a864df3acd73402f9075eeb0852
Richard Joyce <rjoyce@ucdavis.edu>
2017-10-29 21:04:32 -0700

    Basic structure of gh-pages

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dissertation Pages
+
+This is the [github pages](https://rjoyce.me/dissertation/) branch, you probably want the [master](https://github.com/richjoyce/dissertation) branch.
---
+++ make_post.sh
@@ -0,0 +1,20 @@
+SHA=$1
+TAG=$(git describe --tags ${SHA})
+FILENAME=_posts/$(date +"%Y-%m-%d")-${SHA}.md
+SHORT_MESSAGE=`git log -n 1 --pretty=format:%s ${SHA}`
+
+mkdir -p _posts
+
+echo "---" > $FILENAME
+echo "layout: post" >> $FILENAME
+echo "title: ${SHORT_MESSAGE}" >> $FILENAME
+echo "date: `git show -s --format=%ci ${SHA}`" >> $FILENAME
+echo "sha: ${SHA}" >> $FILENAME
+if [ -z $TAG ]; then
+    echo "categories: commit" >> $FILENAME
+else
+    echo "tag: $TAG" >> $FILENAME
+    echo "categories: commit stable" >> $FILENAME
+fi
+echo "---" >> $FILENAME
+git log -n 1 --pretty=format:%B $SHA >> $FILENAME
