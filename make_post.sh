4251d968a6c4f8dd8e4afd3928ce7a06d47b6b81
Richard Joyce <rjoyce@ucdavis.edu>
2017-10-30 21:51:51 -0700

    Use git date for post filename

--- make_post.sh
+++ make_post.sh
@@ -2,7 +2,7 @@
 
 SHA=$1
 TAG=$(git describe --tags --exact-match --abbrev=0 ${SHA})
-FILENAME=_posts/$(date +"%Y-%m-%d")-${SHA}.md
+FILENAME=_posts/$(date --date="$(git show -s --format=%ci ${SHA})" +"%Y-%m-%d")-${SHA}.md
 SHORT_MESSAGE=`git log -n 1 --pretty=format:%s ${SHA}`
 
 mkdir -p _posts
